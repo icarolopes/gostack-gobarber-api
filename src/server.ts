9d1f565e571a4a2c7585eb83e7f6f59ff3e6ba5d
icarolopes <icarofernandeslopes@gmail.com>
2020-08-13 23:35:35 -0300

    1 - Configurando Estrutura

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+ import express from 'express'
+
+ const app = express()
+
+
+ app.get('/', (request, response) => {
+   return response.json({ message: 'Hello GoStack!' })
+ })
+
+ app.listen(3333, () => {
+   console.log('Server starded on port 3333!')
+ })
