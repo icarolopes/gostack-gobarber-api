6ba1d5960d22299c08b1a3c3132bca0e1c26afee
icarolopes <icarofernandeslopes@gmail.com>
2020-08-16 18:46:07 -0300

    alinhando o projeto com o modulo 2 não existente no download do curso

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,38 @@
+import { startOfHour } from 'date-fns'
+import Appointment from '../models/Appointment'
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
+
+interface RequestDTO {
+  provider: string
+  date: Date
+}
+
+class CreateAppointmentService {
+  // private appointmentsRepository: AppointmentsRepository
+
+  // cosntructor(appointmentsRepository: AppointmentsRepository) {
+  //   t
+  // }
+
+  constructor(private appointmentsRepository: AppointmentsRepository) {}
+
+  public execute({ date, provider }: RequestDTO): Appointment {
+    const appointmentDate = startOfHour(date)
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate
+    )
+
+    if (findAppointmentInSameDate) {
+      throw Error('This time is already booked.')
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate
+    })
+
+    return appointment
+  }
+}
+
+export default CreateAppointmentService
