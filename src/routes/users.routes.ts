bcda018218bfdba83d6e15d5bef323c9977de968
icarolopes <icarofernandeslopes@gmail.com>
2020-08-21 23:50:06 -0300

    2 - Atualizando Avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import { CreateUserService } from '../services/CreateUserService'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 import multer from 'multer'
 import { uploadConfig } from '../config/upload'
+import { UpdateUserAvatarService } from '../services/UpdateUserAvatarService'
 
 export const usersRouter = Router()
 const upload = multer(uploadConfig)
@@ -32,6 +33,19 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({ ok: true })
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService()
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename
+      })
+
+      delete user.password
+
+      return response.json(user)
+    } catch (err) {
+      return response.status(400).json({ error: err.message })
+    }
   }
 )
