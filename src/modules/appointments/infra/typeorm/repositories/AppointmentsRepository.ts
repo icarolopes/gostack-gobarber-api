d9c4705bfbf7571bfaeb22f19875d5b849402199
icarolopes <icarofernandeslopes@gmail.com>
2020-08-23 22:38:58 -0300

    6 - Liskov Substitution Principle

---
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -0,0 +1,13 @@
+import { EntityRepository, Repository } from 'typeorm'
+import Appointment from '../entities/Appointment'
+import { IAppointmentsRepository } from '@modules/appointments/repositories/IAppointmentsRepository'
+
+@EntityRepository(Appointment)
+export class AppointmentsRepository extends Repository<Appointment>
+  implements IAppointmentsRepository {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = await this.findOne({ where: { date } })
+
+    return findAppointment
+  }
+}
