214d20503c012749c0d169e9637cf403a53885af
icarolopes <icarofernadeslopes@gmail.com>
2020-09-05 15:44:09 -0300

    6 - Testes de Agendamento

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -4,13 +4,14 @@ import { Appointment } from '@modules/appointments/infra/typeorm/entities/Appoin
 import { ICreateAppointmentDTO } from '@modules/appointments/dtos/ICreateAppointmentDTO'
 
 import { IAppointmentsRepository } from '../IAppointmentsRepository'
+import { isEqual } from 'date-fns'
 
 export class FakeAppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = []
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(
-      appointment => appointment.date === date
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date)
     )
 
     return findAppointment
