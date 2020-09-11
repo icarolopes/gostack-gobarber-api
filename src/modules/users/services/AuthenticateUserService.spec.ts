3faa614a8439533c2d5991d8efa2e62abaf440cb
icarolopes <icarofernadeslopes@gmail.com>
2020-09-10 23:57:50 -0300

    9 - Teste de Autenticação

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -2,6 +2,7 @@ import { FakeUsersRepository } from '../repositories/fakes/FakeUsersRepository'
 import { CreateUserService } from './CreateUserService'
 import { AuthenticateUserService } from './AuthenticateUserService'
 import { FakeHashProvider } from '../provider/HashProvider/fakes/FakeHashProvider'
+import { AppError } from '@shared/errors/AppError'
 
 describe('AuthenticateUser', () => {
   it('should be able to authenticate', async () => {
@@ -30,5 +31,50 @@ describe('AuthenticateUser', () => {
 
     expect(response).toHaveProperty('token')
     expect(response.user).toEqual(user)
+  })
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    )
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@email.com',
+        password: '123123'
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    )
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    )
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@exemple.com',
+      password: '123123'
+    })
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@exemple.com',
+        password: 'wrong-password'
+      })
+    ).rejects.toBeInstanceOf(AppError)
   })
 })
